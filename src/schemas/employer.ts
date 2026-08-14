adeaa91674d04637e977696d20086a3dc3d84e6f
DaphnePap <daphnepapadop@gmail.com>
2026-08-15 01:43:15 +0300

    feat: add Employer schema and type

---
+++ src/schemas/employer.ts
@@ -0,0 +1,52 @@
+import {z} from "zod";
+
+export const employerSchema = z.object({
+    id: z
+        .bigint()
+        .min(1n),
+    uuid: z.uuid(),
+    brandName: z
+        .string()
+        .min(2, {error: "Must have at least 2 characters"}),
+    vat: z
+        .string()
+        .regex(/^\d{9}$/, {error: "Must be exactly 9 digits"}),
+    website: z
+        .string()
+        .url()
+        .max(255, {error: "No longer than 255 characters"})
+        .optional(),
+    professionalFieldId: z
+        .bigint()
+        .min(1n, {error: "Please select an option"}),
+    email: z
+        .email(),
+    telephoneNumber: z
+        .string()
+        .regex(/^\d{10}$/, {error: "Must be exactly 10 digits"}),
+    address: z
+        .string()
+        .optional(),
+    regionId: z
+        .bigint()
+        .min(1n, {error: "Please select an option"}),
+    username: z
+        .string()
+        .min(3, {error: "Must be at least 3 characters"})
+        .max(30, {error: "Must be no longer than 30 characters"}),
+    password: z
+        .string()
+        .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&+=]).{8,}$/,
+            {error: "Must be at least 8 characters, consisting of one capital letter, " +
+                    "one lowercase letter, one digit and one special character"}),
+    createdAt: z
+        .string(),
+    updatedAt: z
+        .string(),
+    deleted: z
+        .boolean(),
+    deletedAt: z
+        .string()
+})
+
+export type Employer = z.infer<typeof employerSchema>;
