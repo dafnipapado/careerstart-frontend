0e123238850f39cb367a8ea171b5f88a01f09465
DaphnePap <daphnepapadop@gmail.com>
2026-08-20 23:50:30 +0300

    feat: add login schema and type

---
+++ src/schemas/auth.ts
@@ -0,0 +1,12 @@
+import {z} from "zod";
+
+export const loginSchema = z.object({
+    username: z
+        .string()
+        .min(1, {error: "Username is required"}),
+    password: z
+        .string()
+        .min(1, {error: "Password is required"})
+})
+
+export type LoginCredentials = z.infer<typeof loginSchema>;
