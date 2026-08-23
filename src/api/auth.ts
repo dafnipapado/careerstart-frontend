5d19a6900ce240a1ca32dcd118475865a3d8ae82
DaphnePap <daphnepapadop@gmail.com>
2026-08-23 23:41:44 +0300

    feat: add login API

---
+++ src/api/auth.ts
@@ -0,0 +1,13 @@
+import type {LoginCredentials, LoginResponse} from "../schemas/auth.ts";
+
+const API_URL = import.meta.env.VITE_API_URL
+
+export async function login(data: LoginCredentials) : Promise<LoginResponse> {
+    const res = await fetch(`${API_URL}/auth/login`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    })
+    if (!res.ok) throw new Error("Failed to login")
+    return await res.json()
+}
