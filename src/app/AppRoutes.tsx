b32c8b9eeb9c181b7183500a2d08c6da0bb0668a
DaphnePap <daphnepapadop@gmail.com>
2026-08-27 23:57:16 +0300

    feat: add Layout route

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -5,23 +5,26 @@ import ProtectedRoute from "@/app/ProtectedRoute.tsx";
 import EmployerDashboard from "@/pages/EmployerDashboard.tsx";
 import PublicRoute from "@/app/PublicRoute.tsx";
 import JobSeekerDashboard from "@/pages/JobSeekerDashboard.tsx";
+import Layout from "@/components/layouts/Layout.tsx";
 
 const AppRoutes = () => {
     return (
         <>
             <Routes>
-                <Route element={<PublicRoute/>}>
-                    <Route index element={<HomePage/>}/>
-                    <Route path="login" element={<LoginPage/>}/>
-                </Route>
-                <Route element={<ProtectedRoute allowedRoles={["EMPLOYER"]}/>}>
-                    <Route path="employer">
-                        <Route path="dashboard" element={<EmployerDashboard/>}/>
+                <Route element={<Layout/>}>
+                    <Route element={<PublicRoute/>}>
+                        <Route index element={<HomePage/>}/>
+                        <Route path="login" element={<LoginPage/>}/>
                     </Route>
-                </Route>
-                <Route element={<ProtectedRoute allowedRoles={["JOB_SEEKER"]}/>}>
-                    <Route path="jobseeker">
-                        <Route path="dashboard" element={<JobSeekerDashboard/>}/>
+                    <Route element={<ProtectedRoute allowedRoles={["EMPLOYER"]}/>}>
+                        <Route path="employer">
+                            <Route path="dashboard" element={<EmployerDashboard/>}/>
+                        </Route>
+                    </Route>
+                    <Route element={<ProtectedRoute allowedRoles={["JOB_SEEKER"]}/>}>
+                        <Route path="jobseeker">
+                            <Route path="dashboard" element={<JobSeekerDashboard/>}/>
+                        </Route>
                     </Route>
                 </Route>
             </Routes>
