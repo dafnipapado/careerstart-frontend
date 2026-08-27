87a071329e5aeebbe830bc20b6994564713f2e16
DaphnePap <daphnepapadop@gmail.com>
2026-08-27 18:00:32 +0300

    feat: add AppRoutes for routing

---
+++ src/app/AppRoutes.tsx
@@ -0,0 +1,32 @@
+import {Route, Routes} from "react-router";
+import HomePage from "@/pages/HomePage.tsx";
+import LoginPage from "@/pages/LoginPage.tsx";
+import ProtectedRoute from "@/app/ProtectedRoute.tsx";
+import EmployerDashboard from "@/pages/EmployerDashboard.tsx";
+import PublicRoute from "@/app/PublicRoute.tsx";
+import JobSeekerDashboard from "@/pages/JobSeekerDashboard.tsx";
+
+const AppRoutes = () => {
+    return (
+        <>
+            <Routes>
+                <Route element={<PublicRoute/>}>
+                    <Route index element={<HomePage/>}/>
+                    <Route path="login" element={<LoginPage/>}/>
+                </Route>
+                <Route element={<ProtectedRoute allowedRoles={["EMPLOYER"]}/>}>
+                    <Route path="employer">
+                        <Route path="dashboard" element={<EmployerDashboard/>}/>
+                    </Route>
+                </Route>
+                <Route element={<ProtectedRoute allowedRoles={["JOB_SEEKER"]}/>}>
+                    <Route path="jobseeker">
+                        <Route path="dashboard" element={<JobSeekerDashboard/>}/>
+                    </Route>
+                </Route>
+            </Routes>
+        </>
+    )
+}
+
+export default AppRoutes;
