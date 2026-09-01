915ba2855eebc7999fe9bc0ab705df0c8b9fcc56
DaphnePap <daphnepapadop@gmail.com>
2026-09-01 21:57:54 +0300

    feat: display employer job listings in dashboard

--- src/pages/employer/EmployerDashboardPage.tsx
+++ src/pages/employer/EmployerDashboardPage.tsx
@@ -2,20 +2,36 @@ import defaultUserPicture from "../../assets/images/default-user-picture.png";
 import {useEffect, useState} from "react";
 import {getLoggedInEmployerDetails} from "@/api/employer.ts";
 import type {EmployerReadDetails} from "@/schemas/employer.ts";
-import {Factory, MapPin, Settings, SquareArrowOutUpRight} from "lucide-react";
+import {Dot, Factory, MapPin, Settings, SquareArrowOutUpRight} from "lucide-react";
 import {Link} from "react-router";
 import {Separator} from "@/components/ui/separator.tsx";
+import CustomButton from "@/components/shared/CustomButton.tsx";
+import {getPaginatedFilteredJobListings} from "@/api/jobListing.ts";
+import {defaultJobListingFilters} from "@/schemas/jobListingFilters.ts";
+import type {JobListingReadSummary} from "@/schemas/jobListing.ts";
+import {useAuth} from "@/context/AuthProvider.tsx";
+import {Card, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+import {Button} from "@base-ui/react";
 
 const EmployerDashboardPage = () => {
 
+    const { isAuthenticated } = useAuth()
     const [employerInfo, setEmployerInfo] = useState<EmployerReadDetails | null>(null)
+    const [jobListings, setJobListings] = useState<JobListingReadSummary[]>([])
 
     useEffect(() => {
         const fetchEmployer = async () => {
-            setEmployerInfo(await getLoggedInEmployerDetails())
+            const employerData = await getLoggedInEmployerDetails()
+            setEmployerInfo(employerData)
+            const data = await getPaginatedFilteredJobListings({
+                ...defaultJobListingFilters, employerUuid: employerData.uuid
+            })
+            setJobListings(data.content)
         }
+
         void fetchEmployer()
-    }, []);
+    }, [isAuthenticated]);
+
 
     return (
         <>
@@ -48,6 +64,68 @@ const EmployerDashboardPage = () => {
                     <p>Text Placeholder</p>
                 </div>
                 <Separator className="bg-gray-400 mt-15 mb-10" />
+            </div>
+
+            {/*job listings*/}
+            <div>
+                <div className="text-left left-5 flex justify-between items-center">
+                    <h1 className="text-2xl font-semibold">My Job Listings</h1>
+                    <Link to="/employer/create-joblisting">
+                        <CustomButton label="+ New job listing"></CustomButton>
+                    </Link>
+                </div>
+
+                {jobListings.length > 0 ?
+                <div className="container w-full">
+                    {jobListings.map((jobListing) => (
+                    <Card key={jobListing.uuid} className="flex flex-col mx-auto w-full h-50 p-5 mb-10">
+                        <CardHeader className="flex items-center justify-between">
+                            <CardTitle className="text-2xl">
+                                <div key={jobListing.title}>
+                                    {jobListing.title}
+                                </div>
+                            </CardTitle>
+                            <CardDescription className="text-gray-500">
+                                <div key={jobListing.dateCreated.slice(0,10)}>
+                                    {jobListing.dateCreated.slice(0,10)}
+                                </div>
+                            </CardDescription>
+                        </CardHeader>
+                        <div className="flex flex-col text-base font-sans">
+                            <div className="self-start ml-7 -mt-5 text-sm figtree-custom-italics text-red-800" key={jobListing.professionalFieldName}>
+                                {jobListing.professionalFieldName}
+                            </div>
+                            <div className="flex items-baseline gap-1 ml-5 mt-3 text-sm font-medium">
+                                <span className="flex gap-1">
+                                    <MapPin strokeWidth={1.25} size={20}  />
+                                    <div key={jobListing.regionName}>
+                                        {jobListing.regionName}
+                                    </div>
+                                </span>
+                                <Dot  />
+                                <span className="flex gap-1">
+                                    <Factory strokeWidth={1.25} size={20}  />
+                                    <div key={jobListing.employerBrandName}>
+                                        {jobListing.employerBrandName}
+                                    </div>
+                                </span>
+                            </div>
+                        </div>
+                        <CardFooter className="w-full mt-auto text-font-dark-purple">
+                            <Link to="" className="w-full">
+                                <Button className="w-full border border-font-dark-purple hover:bg-gray-200 px-4 py-2 rounded-sm cursor-pointer">View Listing ⟶</Button>
+                            </Link>
+                        </CardFooter>
+                    </Card>
+                    ))}
+                </div>
+                :
+                <div className="container w-full h-50 border border-gray-400 rounded-md">
+                    <div className="h-full content-center">
+                        <p>You have no active job listings yet.</p>
+                        <Link to="/employer/create-joblisting" className="text-font-link-blue hover:underline">Post your first job listing</Link>
+                    </div>
+                </div>}
             </div>
         </>
     )
