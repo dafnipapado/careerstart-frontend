7d4dec8970301da98ac690e7f4a5f4fb31b764ed
DaphnePap <daphnepapadop@gmail.com>
2026-09-01 21:56:15 +0300

    feat: add API for paginated and filtered job listing retrieval

--- src/api/jobListing.ts
+++ src/api/jobListing.ts
@@ -1,5 +1,7 @@
-import type {JobListingInsert, JobListingRead} from "@/schemas/jobListing.ts";
+import type {JobListingInsert, JobListingRead, JobListingReadSummary} from "@/schemas/jobListing.ts";
 import {authFetch} from "@/api/auth.ts";
+import type {Page} from "@/schemas/page.ts";
+import type {JobListingFilters} from "@/schemas/jobListingFilters.ts";
 
 const API_URL = import.meta.env.VITE_API_URL
 const JOB_LISTING_URL = `${API_URL}/job-listings`
@@ -17,5 +19,17 @@ export async function insertJobListing(data: JobListingInsert) : Promise<JobList
         body: JSON.stringify(requestBody)
     })
     if (!res.ok) throw new Error("Job Listing creation failed")
+    return await res.json()
+}
+
+export async function getPaginatedFilteredJobListings(filters: JobListingFilters) : Promise<Page<JobListingReadSummary>> {
+    const params = new URLSearchParams();
+    Object.entries(filters).forEach(([key, value]) => {
+        if (value !== undefined && value !== null && value!=="") {
+            params.append(key, String(value))
+        }
+    })
+    const res = await authFetch(`${JOB_LISTING_URL}?${params}`)
+    if (!res.ok) throw new Error("Failed to apply filters")
     return await res.json()
 }
