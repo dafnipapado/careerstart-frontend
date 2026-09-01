643b1f0cf4601fcfef1aa83a1e01a41791c9072f
DaphnePap <daphnepapadop@gmail.com>
2026-09-01 21:51:00 +0300

    fix: wrap authentication check in useEffect

--- src/app/PublicRoute.tsx
+++ src/app/PublicRoute.tsx
@@ -1,15 +1,18 @@
 import {useAuth} from "@/context/AuthProvider.tsx";
 import {Outlet, useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 const PublicRoute = () => {
-    const { isAuthenticated } = useAuth();
-    const role = useAuth().role?.toLowerCase();
+    const { isAuthenticated, role } = useAuth();
     const location = useLocation();
     const navigate = useNavigate();
 
-    if (isAuthenticated) {
-        navigate(location.state?.from ?? `/${role}/dashboard`, {replace: true})
-    }
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate(location.state?.from ?? `/${role?.toLowerCase()}/dashboard`, {replace: true})
+        }
+    }, [isAuthenticated, role, location.state, navigate]);
+
 
     return <Outlet/>
 }
