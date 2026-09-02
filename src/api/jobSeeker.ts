2963c3a7146f0405ce41b5e31d580e3d0d6802ea
DaphnePap <daphnepapadop@gmail.com>
2026-09-03 01:05:11 +0300

    feat: add functions for jobseeker update and logged-in jobseeker retrieval

--- src/api/jobSeeker.ts
+++ src/api/jobSeeker.ts
@@ -1,4 +1,5 @@
-import type {JobSeekerInsert, JobSeekerRead} from "@/schemas/jobSeeker.ts";
+import type {JobSeekerInsert, JobSeekerRead, JobSeekerReadDetails, JobSeekerUpdate} from "@/schemas/jobSeeker.ts";
+import {authFetch} from "@/api/auth.ts";
 
 const API_URL = import.meta.env.VITE_API_URL
 const JOBSEEKER_URL = `${API_URL}/jobseekers`
@@ -24,5 +25,36 @@ export async function insertJobSeeker(data: JobSeekerInsert) : Promise<JobSeeker
         body: JSON.stringify(requestBody)
     })
     if (!res.ok) throw new Error("Job Seeker creation failed")
+    return await res.json()
+}
+
+export async function getLoggedInJobSeekerDetails() : Promise<JobSeekerReadDetails> {
+    const res = await authFetch(`${JOBSEEKER_URL}/dashboard`)
+    if (!res.ok) throw new Error("Job Seeker info not found")
+    return await res.json()
+}
+
+export async function updateJobSeeker(data: JobSeekerUpdate) : Promise<JobSeekerRead> {
+    const requestBody = {
+        uuid: data.uuid,
+        firstname: data.firstname,
+        lastname: data.lastname,
+        userUpdateDTO: {
+            username: data.username
+        },
+        personalInfoUpdateDTO: {
+            email: data.email,
+            telephoneNumber: data.telephoneNumber || null,
+            address: data.address || null,
+            regionId: data.regionId
+        }
+    }
+
+    const res = await authFetch(`${JOBSEEKER_URL}/${data.uuid}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(requestBody)
+    })
+    if (!res.ok) throw new Error("Job seeker update failed")
     return await res.json()
 }
