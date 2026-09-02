38c646b67195e93f54ab1b12e3850a3cbe7e8db8
DaphnePap <daphnepapadop@gmail.com>
2026-09-02 08:49:22 +0300

    fix: fill link paths and add z-index styling to header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -5,21 +5,21 @@ const Header = () => {
 
     return (
         <>
-            <div className="fixed w-full h-13 top-0 left-0 flex justify-center bg-black">
+            <div className="fixed w-full h-13 top-0 left-0 flex justify-center bg-black z-50">
                 <div className="w-[55%] flex justify-end items-center ">
                     <AuthButton/>
                 </div>
 
             </div>
-            <header className="fixed w-full h-30 left-0 flex justify-center bg-white px-8 py-4 mt-13">
+            <header className="fixed w-full h-30 left-0 flex justify-center bg-white px-8 py-4 mt-13 z-50">
                 <div className="w-[55%] flex items-center justify-between">
                     <Link to="/">
                         <h1 className="lobster-two-regular-italic text-6xl text-font-dark-purple w-70">CareerStart</h1>
                     </Link>
                     <nav>
                          <div className="flex pt-5 gap-8 text-font-dark-purple text-xl font-semibold font-sans">
-                            <Link to="/">Browse job listings</Link>
-                            <Link to="/">Post a job listing</Link>
+                            <Link to="/job-listings">Browse job listings</Link>
+                            <Link to="/employer/create-joblisting">Post a job listing</Link>
                          </div>
                     </nav>
                 </div>
