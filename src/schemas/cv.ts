19f8a1663060c0349d471fab2a046147dc6f72aa
DaphnePap <daphnepapadop@gmail.com>
2026-09-04 01:05:08 +0300

    feat: add schemas and types for CV insert and update

---
+++ src/schemas/cv.ts
@@ -0,0 +1,56 @@
+import {z} from "zod";
+
+export const cvSchema = z.object({
+    id: z
+        .number()
+        .min(1),
+    uuid: z
+        .uuid(),
+    profession: z
+        .string()
+        .min(3, {error: "Must have at least 3 characters"}),
+    bio: z
+        .string()
+        .optional(),
+    education: z
+        .string()
+        .optional(),
+    experience: z
+        .string()
+        .optional(),
+    certificates: z
+        .string()
+        .optional(),
+    languages: z
+        .string()
+        .optional(),
+    skills: z
+        .string()
+        .optional(),
+    jobSeekerId: z
+        .number()
+        .min(1)
+})
+
+export type Cv = z.infer<typeof cvSchema>
+
+export const cvInsertSchema = cvSchema.omit({
+    id: true,
+    uuid: true,
+    jobSeekerId: true
+})
+
+export type cvInsert = z.infer<typeof cvInsertSchema>
+
+export const cvUpdateSchema = cvSchema.omit({
+    id: true,
+    jobSeekerId: true
+})
+
+export type cvUpdate = z.infer<typeof cvUpdateSchema>
+
+export const cvReadSchema = cvSchema.omit({
+    id: true
+})
+
+export type cvRead = z.infer<typeof cvReadSchema>
