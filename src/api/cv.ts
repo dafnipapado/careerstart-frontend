952a7fb14acb08bb8bc40d80b44e7c374ba6d3ec
DaphnePap <daphnepapadop@gmail.com>
2026-09-04 23:49:38 +0300

    feat: add API for cv update and retrieval

--- src/api/cv.ts
+++ src/api/cv.ts
@@ -1,15 +1,31 @@
-import type {cvInsert, cvRead} from "@/schemas/cv.ts";
+import type {CvInsert, CvRead, CvUpdate} from "@/schemas/cv.ts";
 import {authFetch} from "@/api/auth.ts";
 
 const API_URL = import.meta.env.VITE_API_URL
 const CV_URL = `${API_URL}/cv`
 
-export async function insertCv(data: cvInsert) : Promise<cvRead> {
+export async function insertCv(data: CvInsert) : Promise<CvRead> {
     const res = await authFetch(`${CV_URL}`, {
-        method: 'POST',
+        method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(data)
     })
     if (!res.ok) throw new Error("Cv creation failed")
+    return await res.json()
+}
+
+export async function updateCv(data: CvUpdate) : Promise<CvRead> {
+    const res = await authFetch(`${CV_URL}/${data.uuid}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    })
+    if (!res.ok) throw new Error("Cv update failed")
+    return await res.json()
+}
+
+export async function getJobSeekerCv(jobSeekerUuid: string) : Promise<CvRead> {
+    const res = await authFetch(`${CV_URL}/${jobSeekerUuid}/view`)
+    if (!res.ok) throw new Error("Cv retrieval failed")
     return await res.json()
 }
