c72d6e48c1fb6954978c6c2e02926da7fb7790d0
DaphnePap <daphnepapadop@gmail.com>
2026-09-11 15:16:19 +0300

    feat: control header buttons visibility and update styling

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,25 +1,31 @@
 import {Link} from "react-router";
 import {AuthButton} from "../shared/AuthButton.tsx";
+import {useAuth} from "@/context/AuthProvider.tsx";
 
 const Header = () => {
 
+    const {role} = useAuth();
+
     return (
         <>
-            <div className="fixed w-full h-13 top-0 left-0 flex justify-center bg-black z-50">
-                <div className="w-[55%] flex justify-end items-center ">
+            <div className="fixed w-full h-13 top-0 left-0 flex justify-center bg-secondary-light-purple z-50">
+                <div className="w-[60%] flex justify-end items-center ">
                     <AuthButton/>
                 </div>
 
             </div>
-            <header className="fixed w-full h-30 left-0 flex justify-center bg-white px-8 py-4 mt-13 z-50">
-                <div className="w-[55%] flex items-center justify-between">
+            <header className="fixed w-full h-30 left-0 flex justify-center bg-white px-8 mt-13 z-50">
+                <div className="w-[60%] flex items-center justify-between">
                     <Link to="/">
-                        <h1 className="lobster-two-regular-italic text-6xl text-font-dark-purple w-70">CareerStart</h1>
+                        <h1 className="lobster-two-regular-italic text-6xl text-primary-dark-purple w-70">CareerStart</h1>
                     </Link>
                     <nav>
-                         <div className="flex pt-5 gap-8 text-font-dark-purple text-xl font-semibold font-sans">
+                         <div className="flex pt-5 gap-8 text-primary-dark-purple text-xl font-semibold font-sans">
                             <Link to="/job-listings">Browse job listings</Link>
-                            <Link to="/employer/create-joblisting">Post a job listing</Link>
+                             {role === "JOB_SEEKER"
+                             ? <Link to="/job_seeker/my-applications">My job applications</Link>
+                             : <Link to="/employer/create-joblisting">Post a job listing</Link>
+                             }
                          </div>
                     </nav>
                 </div>
