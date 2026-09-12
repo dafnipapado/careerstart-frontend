9946f675b95f32fd0df0983f3a3f6c1c58e2973f
DaphnePap <daphnepapadop@gmail.com>
2026-09-12 16:48:55 +0300

    feat: add jobseeker and job listing management pages for admin

---
+++ src/pages/admin/JobListingManagementPage.tsx
@@ -0,0 +1,96 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import {useEffect, useState} from "react";
+import type {Pagination} from "@/schemas/pagination.ts";
+import {CircleCheck, CircleX} from "lucide-react";
+import {Button} from "@/components/ui/button.tsx";
+import type {ErrorResponse} from "@/schemas/error.ts";
+import {toast} from "sonner";
+import {deleteJobListing, getPaginatedFilteredJobListings, restoreJobListing} from "@/api/jobListing.ts";
+import {defaultJobListingFilters} from "@/schemas/jobListingFilters.ts";
+import type {JobListingReadSummary} from "@/schemas/jobListing.ts";
+
+const JobListingManagementPage = () => {
+
+    const [jobListingInfo, setJobListingInfo] = useState<Pagination<JobListingReadSummary> | null>(null);
+    const [refresh, setRefresh] = useState(false)
+
+    useEffect(() => {
+        const getJobListingData = async () => {
+            const jobListingData = await getPaginatedFilteredJobListings({...defaultJobListingFilters})
+            setJobListingInfo(jobListingData)
+        }
+
+        void getJobListingData()
+    }, [refresh]);
+
+    const handleDelete = async (uuid: string) => {
+        if (!window.confirm("Are you sure you want to delete this job listing?")) return
+        try {
+            await deleteJobListing(uuid)
+            setRefresh((prev) => !prev)
+            toast.success("Job listing deleted successfully")
+        } catch (error) {
+            const err = error as ErrorResponse
+            toast.error(err.message)
+        }
+    }
+
+    const handleRestore = async (uuid: string) => {
+        try {
+            await restoreJobListing(uuid)
+            setRefresh((prev) => !prev)
+            toast.success("Job listing restored successfully")
+        } catch (error) {
+            const err = error as ErrorResponse
+            toast.error(err.message)
+        }
+    }
+
+    return (
+        <>
+            <Table className="w-full mt-10">
+                <TableHeader>
+                    <TableHead>Job Title</TableHead>
+                    <TableHead>Employer</TableHead>
+                    <TableHead>Industry</TableHead>
+                    <TableHead>Region</TableHead>
+                    <TableHead>Created At</TableHead>
+                    <TableHead>Deleted</TableHead>
+                    <TableHead>Actions</TableHead>
+                </TableHeader>
+                <TableBody>
+                    {jobListingInfo?.content.map(listing => (
+                        <TableRow>
+                            <TableCell>{listing.title}</TableCell>
+                            <TableCell>{listing.employerBrandName}</TableCell>
+                            <TableCell>{listing.professionalFieldName}</TableCell>
+                            <TableCell>{listing.regionName}</TableCell>
+                            <TableCell>{listing.dateCreated.slice(0, 10)}</TableCell>
+                            <TableCell className="flex justify-center">{listing.deleted ? <CircleCheck/> : <CircleX/>}</TableCell>
+                            <TableCell>
+                                <Button onClick={() =>
+                                    listing.deleted
+                                        ? handleRestore(listing.uuid)
+                                        : handleDelete(listing.uuid)}
+                                        className={`${listing.deleted ? "border border-green-800 text-green-800" : "border border-red-800 text-red-800"}
+                                            rounded-sm cursor-pointer`}
+                                >
+                                    {listing.deleted ? "Restore" : "Delete"}
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </>
+    )
+}
+
+export default JobListingManagementPage
---
+++ src/pages/admin/JobSeekerManagementPage.tsx
@@ -0,0 +1,92 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import {useEffect, useState} from "react";
+import type {Pagination} from "@/schemas/pagination.ts";
+import {CircleCheck, CircleX} from "lucide-react";
+import {Button} from "@/components/ui/button.tsx";
+import type {ErrorResponse} from "@/schemas/error.ts";
+import {toast} from "sonner";
+import type {JobSeekerReadDetails} from "@/schemas/jobSeeker.ts";
+import {activateJobSeeker, deleteJobSeeker, getPaginatedFilteredJobSeekers} from "@/api/jobSeeker.ts";
+import {defaultJobSeekerFilters} from "@/schemas/jobSeekerFilters.ts";
+
+const JobSeekerManagementPage = () => {
+
+    const [jobSeekerInfo, setJobSeekerInfo] = useState<Pagination<JobSeekerReadDetails> | null>(null);
+    const [refresh, setRefresh] = useState(false)
+
+    useEffect(() => {
+        const getJobSeekerData = async () => {
+            const jobSeekerData = await getPaginatedFilteredJobSeekers({...defaultJobSeekerFilters})
+            setJobSeekerInfo(jobSeekerData)
+        }
+
+        void getJobSeekerData()
+    }, [refresh]);
+
+    const handleDelete = async (uuid: string) => {
+        if (!window.confirm("Are you sure you want to deactive this job seeker account?")) return
+        try {
+            await deleteJobSeeker(uuid)
+            setRefresh((prev) => !prev)
+            toast.success("Job seeker deactivated successfully")
+        } catch (error) {
+            const err = error as ErrorResponse
+            toast.error(err.message)
+        }
+    }
+
+    const handleActivate = async (uuid: string) => {
+        try {
+            await activateJobSeeker(uuid)
+            setRefresh((prev) => !prev)
+            toast.success("Job seeker activated successfully")
+        } catch (error) {
+            const err = error as ErrorResponse
+            toast.error(err.message)
+        }
+    }
+
+    return (
+        <>
+            <Table className="w-full mt-10">
+                <TableHeader>
+                    <TableHead>Firstname</TableHead>
+                    <TableHead>Lastname</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Deleted</TableHead>
+                    <TableHead>Actions</TableHead>
+                </TableHeader>
+                <TableBody>
+                    {jobSeekerInfo?.content.map(jobSeeker => (
+                        <TableRow>
+                            <TableCell>{jobSeeker.firstname}</TableCell>
+                            <TableCell>{jobSeeker.lastname}</TableCell>
+                            <TableCell>{jobSeeker.personalInfoDetailsReadOnlyDTO.email}</TableCell>
+                            <TableCell className="flex justify-center">{jobSeeker.deleted ? <CircleCheck/> : <CircleX/>}</TableCell>
+                            <TableCell>
+                                <Button onClick={() =>
+                                    jobSeeker.deleted
+                                        ? handleActivate(jobSeeker.uuid)
+                                        : handleDelete(jobSeeker.uuid)}
+                                        className={`${jobSeeker.deleted ? "border border-green-800 text-green-800" : "border border-red-800 text-red-800"}
+                                            rounded-sm cursor-pointer`}
+                                >
+                                    {jobSeeker.deleted ? "Activate" : "Deactivate"}
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </>
+    )
+}
+
+export default JobSeekerManagementPage
